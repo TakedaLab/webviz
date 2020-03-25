eb34f8633d7f4569b9932e6c07b294c7266ae39d
d-hayashi <hayashi.daiki@nagoya-u.jp>
2020-03-25 19:22:46 +0900

    Add an example use of global variables

--- packages/webviz-core/src/panels/HDL/index.js
+++ packages/webviz-core/src/panels/HDL/index.js
@@ -5,6 +5,7 @@
 //  This source code is licensed under the Apache License, Version 2.0,
 //  found in the LICENSE file in the root directory of this source tree.
 //  You may not use this file except in compliance with the License.
+import { partition } from "lodash";
 import * as React from "react";
 import { hot } from "react-hot-loader/root";
 import styled from "styled-components";
@@ -12,6 +13,9 @@ import styled from "styled-components";
 import TextContent from "../../components/TextContent";
 import Panel from "webviz-core/src/components/Panel";
 import PanelToolbar from "webviz-core/src/components/PanelToolbar";
+import useGlobalVariables from "webviz-core/src/hooks/useGlobalVariables";
+import { memoizedGetLinkedGlobalVariablesKeyByName } from "webviz-core/src/panels/ThreeDimensionalViz/Interactions/interactionUtils";
+import useLinkedGlobalVariables from "webviz-core/src/panels/ThreeDimensionalViz/Interactions/useLinkedGlobalVariables";
 
 // HDL Test Panel
 const Container = styled.div`
@@ -35,11 +39,35 @@ const Container = styled.div`
 `;
 
 function HDLTestPanel(): React.Node {
+  const { globalVariables, _0, _1 } = useGlobalVariables();
+  const { linkedGlobalVariables } = useLinkedGlobalVariables();
+  const globalVariableNames = Object.keys(globalVariables);
+  const linkedGlobalVariablesKeyByName = memoizedGetLinkedGlobalVariablesKeyByName(linkedGlobalVariables);
+  const [linked, unlinked] = partition(globalVariableNames, (name) => !!linkedGlobalVariablesKeyByName[name]);
+
   return (
     <Container>
       <PanelToolbar floating />
       <h1>HDL Test Panel</h1>
-      <TextContent>This is a test panel.</TextContent>
+      <TextContent>This is a test panel. aaa</TextContent>
+      {linked.map((name, idx) => {
+        return (
+          <tr key={`linked-${idx}`}>
+            <a>
+              {name}: {JSON.stringify(globalVariables[name] ?? "")}
+            </a>
+          </tr>
+        );
+      })}
+      {unlinked.map((name, idx) => {
+        return (
+          <tr key={`unlinked-${idx}`}>
+            <a>
+              {name}: {JSON.stringify(globalVariables[name] ?? "")}
+            </a>
+          </tr>
+        );
+      })}
     </Container>
   );
 }
